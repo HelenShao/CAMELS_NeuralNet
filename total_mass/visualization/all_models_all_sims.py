00a74fa59fbdb4c3eac486415bc2a95dbce310a2
HelenShao <52927975+HelenShao@users.noreply.github.com>
2021-08-08 15:03:42 -0400

    Update all_models_all_sims.py

--- total_mass/visualization/all_models_all_sims.py
+++ total_mass/visualization/all_models_all_sims.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+
 fig = plt.figure(figsize=(28, 40))
 fig.tight_layout()
 plt.rcParams['axes.linewidth'] = 1.5
@@ -92,14 +96,14 @@ plt.plot(np.full((17), 10), np.arange(17), color="black", linestyle = "--")
 plt.plot(np.full((17), 12), np.arange(17), color="black", linestyle = "--")
 
 # Text
-plt.text(6.6, 15, "MSE: 1.57e-3", fontsize=16)
-plt.text(6.55, 14.7, " %.2f"%(percentage_S)+"%", fontsize=16)
+plt.text(6.6, 15, "1.57e-3", fontsize=23)
+plt.text(6.52, 14.55, " %.2f"%(percentage_S)+"%", fontsize=23)
 
-plt.text(10.1, 15, "MSE: 1.55e-3", fontsize=16)
-plt.text(10.05, 14.7, " %.2f"%(percentage_M)+"%", fontsize=16)
+plt.text(10.1, 15, "1.55e-3", fontsize=23)
+plt.text(10, 14.55, " %.2f"%(percentage_M)+"%", fontsize=23)
 
-plt.text(12.1, 15, "MSE: 1.82e-3", fontsize=16)
-plt.text(12.05, 14.7, " %.2f"%(percentage_L)+"%", fontsize=16)
+plt.text(12.1, 15, "1.82e-3", fontsize=23)
+plt.text(12.05, 14.55, " %.2f"%(percentage_L)+"%", fontsize=23)
 
 textstr = "MSE: 1.57e-3 \n%.2f"%(percentage_T)+"%"
 props = dict(boxstyle='square', facecolor='white', alpha=0.5)
@@ -202,14 +206,14 @@ plt.plot(np.full((17), 10), np.arange(17), color="black", linestyle = "--")
 plt.plot(np.full((17), 12), np.arange(17), color="black", linestyle = "--")
 
 # Text
-plt.text(6.6, 15, "MSE: 2.08-e3", fontsize=16)
-plt.text(6.55, 14.7, " %.2f"%(percentage_S)+"%", fontsize=16)
+plt.text(6.6, 15, "2.08-e3", fontsize=23)
+plt.text(6.52, 14.55, " %.2f"%(percentage_S)+"%", fontsize=23)
 
-plt.text(10.1, 15, "MSE: 7.39e-4", fontsize=16)
-plt.text(10.05, 14.7, " %.2f"%(percentage_M)+"%", fontsize=16)
+plt.text(10.1, 15, "7.39e-4", fontsize=23)
+plt.text(10.05, 14.55, " %.2f"%(percentage_M)+"%", fontsize=23)
 
-plt.text(12.1, 15, "MSE: 9.26e-4", fontsize=16)
-plt.text(12.05, 14.7, " %.2f"%(percentage_L)+"%", fontsize=16)
+plt.text(12.1, 15, "9.26e-4", fontsize=23)
+plt.text(12.05, 14.55, " %.2f"%(percentage_L)+"%", fontsize=23)
 
 textstr = "MSE: 8.75e-4 \n%.2f"%(percentage_T)+"%"
 props = dict(boxstyle='square', facecolor='white', alpha=0.5)
@@ -311,14 +315,14 @@ plt.plot(np.full((17), 10), np.arange(17), color="black", linestyle = "--")
 plt.plot(np.full((17), 12), np.arange(17), color="black", linestyle = "--")
 
 # Text
-plt.text(6.6, 15, "MSE: 6.87e-4", fontsize=16)
-plt.text(6.55, 14.7, " %.2f"%(percentage_S)+"%", fontsize=16)
+plt.text(6.6, 15, "6.87e-4", fontsize=23)
+plt.text(6.52, 14.55, " %.2f"%(percentage_S)+"%", fontsize=23)
 
-plt.text(10.1, 15, "MSE: 4.17e-4", fontsize=16)
-plt.text(10.05, 14.7, " %.2f"%(percentage_M)+"%", fontsize=16)
+plt.text(10.1, 15, "4.17e-4", fontsize=23)
+plt.text(10, 14.55, " %.2f"%(percentage_M)+"%", fontsize=23)
 
-plt.text(12.1, 15, "MSE: 4.27e-4", fontsize=16)
-plt.text(12.05, 14.7, " %.2f"%(percentage_L)+"%", fontsize=16)
+plt.text(12.1, 15, "4.27e-4", fontsize=23)
+plt.text(12.05, 14.55, " %.2f"%(percentage_L)+"%", fontsize=23)
 
 textstr = "MSE: 4.42e-4 \n%.2f"%(percentage_T)+"%"
 props = dict(boxstyle='square', facecolor='white', alpha=0.5)
@@ -343,8 +347,8 @@ large_pred = np.load("new_model_l2_SIMBA_pred.npy")
 med_true = np.load("new_model_m_SIMBA_true.npy")
 med_pred = np.load("new_model_m_SIMBA_pred.npy")
 
-small_true = np.load("new_model_s3_SIMBA_true.npy")
-small_pred = np.load("new_model_s3_SIMBA_pred.npy")
+small_true = np.load("new_model_s5_SIMBA_true.npy")
+small_pred = np.load("new_model_s5_SIMBA_pred.npy")
 
 total_true = np.concatenate((small_true, med_true, large_true))
 total_pred = np.concatenate((small_pred, med_pred, large_pred))
@@ -423,14 +427,14 @@ plt.plot(np.full((17), 10), np.arange(17), color="black", linestyle = "--")
 plt.plot(np.full((17), 12), np.arange(17), color="black", linestyle = "--")
 
 # Text
-plt.text(6.6, 15, "MSE: 3.91e-3", fontsize=16)
-plt.text(6.55, 14.7, " %.2f"%(percentage_S)+"%", fontsize=16)
+plt.text(6.6, 15, "3.91e-3", fontsize=23)
+plt.text(6.52, 14.55, " %.2f"%(percentage_S)+"%", fontsize=23)
 
-plt.text(10.1, 15, "MSE: 1.99e-2", fontsize=16)
-plt.text(10.05, 14.7, " %.2f"%(percentage_M)+"%", fontsize=16)
+plt.text(10.1, 15, "1.99e-2", fontsize=23)
+plt.text(10, 14.55, " %.2f"%(percentage_M)+"%", fontsize=23)
 
-plt.text(12.1, 15, "MSE: 4.65e-3", fontsize=16)
-plt.text(12.05, 14.7, " %.2f"%(percentage_L)+"%", fontsize=16)
+plt.text(12.1, 15, "4.65e-3", fontsize=23)
+plt.text(12.05, 14.55, " %.2f"%(percentage_L)+"%", fontsize=23)
 
 textstr = "MSE: 1.83e-2 \n%.2f"%(percentage_T)+"%"
 props = dict(boxstyle='square', facecolor='white', alpha=0.5)
@@ -530,14 +534,14 @@ plt.plot(np.full((17), 10), np.arange(17), color="black", linestyle = "--")
 plt.plot(np.full((17), 12), np.arange(17), color="black", linestyle = "--")
 
 # Text
-plt.text(6.6, 15, "MSE: 3.70e-3", fontsize=16)
-plt.text(6.55, 14.7, " %.2f"%(percentage_S)+"%", fontsize=16)
+plt.text(6.6, 15, "3.70e-3", fontsize=16)
+plt.text(6.52, 14.55, " %.2f"%(percentage_S)+"%", fontsize=23)
 
-plt.text(10.1, 15, "MSE: 4.57e-3", fontsize=16)
-plt.text(10.05, 14.7, " %.2f"%(percentage_M)+"%", fontsize=16)
+plt.text(10.1, 15, "4.57e-3", fontsize=23)
+plt.text(10, 14.55, " %.2f"%(percentage_M)+"%", fontsize=23)
 
-plt.text(12.1, 15, "MSE: 2.60e-3", fontsize=16)
-plt.text(12.05, 14.7, " %.2f"%(percentage_L)+"%", fontsize=16)
+plt.text(12.1, 15, "2.60e-3", fontsize=23)
+plt.text(12.05, 14.55, " %.2f"%(percentage_L)+"%", fontsize=23)
 
 textstr = "MSE: 4.43e-3 \n%.2f"%(percentage_T)+"%"
 props = dict(boxstyle='square', facecolor='white', alpha=0.5)
@@ -637,14 +641,14 @@ plt.plot(np.full((17), 10), np.arange(17), color="black", linestyle = "--")
 plt.plot(np.full((17), 12), np.arange(17), color="black", linestyle = "--")
 
 # Text
-plt.text(6.6, 15, "MSE: 3.74e-3", fontsize=16)
-plt.text(6.55, 14.7, " %.2f"%(percentage_S)+"%", fontsize=16)
+plt.text(6.6, 15, "3.74e-3", fontsize=23)
+plt.text(6.52, 14.55, " %.2f"%(percentage_S)+"%", fontsize=23)
 
-plt.text(10.1, 15, "MSE: 4.82e-3", fontsize=16)
-plt.text(10.05, 14.7, " %.2f"%(percentage_M)+"%", fontsize=16)
+plt.text(10.1, 15, "4.82e-3", fontsize=23)
+plt.text(10, 14.55, " %.2f"%(percentage_M)+"%", fontsize=23)
 
-plt.text(12.1, 15, "MSE: 3.67e-3", fontsize=16)
-plt.text(12.05, 14.7, " %.2f"%(percentage_L)+"%", fontsize=16)
+plt.text(12.1, 15, "3.67e-3", fontsize=23)
+plt.text(12.05, 14.55, " %.2f"%(percentage_L)+"%", fontsize=23)
 
 textstr = "MSE: 4.70e-3 \n%.2f"%(percentage_T)+"%"
 props = dict(boxstyle='square', facecolor='white', alpha=0.5)
@@ -746,14 +750,14 @@ plt.plot(np.full((17), 10), np.arange(17), color="black", linestyle = "--")
 plt.plot(np.full((17), 12), np.arange(17), color="black", linestyle = "--")
 
 # Text
-plt.text(6.6, 15, "MSE: 1.36e-3", fontsize=16)
-plt.text(6.55, 14.7, " %.2f"%(percentage_S)+"%", fontsize=16)
+plt.text(6.6, 15, "1.36e-3", fontsize=23)
+plt.text(6.52, 14.55, " %.2f"%(percentage_S)+"%", fontsize=23)
 
-plt.text(10.1, 15, "MSE: 7.28e-4", fontsize=16)
-plt.text(10.05, 14.7, " %.2f"%(percentage_M)+"%", fontsize=16)
+plt.text(10.1, 15, "7.28e-4", fontsize=23)
+plt.text(10, 14.55, " %.2f"%(percentage_M)+"%", fontsize=23)
 
-plt.text(12.1, 15, "MSE: 1.44e-3", fontsize=16)
-plt.text(12.05, 14.7, " %.2f"%(percentage_L)+"%", fontsize=16)
+plt.text(12.1, 15, "1.44e-3", fontsize=23)
+plt.text(12.05, 14.55, " %.2f"%(percentage_L)+"%", fontsize=23)
 
 textstr = "MSE: 9.48e-4 \n%.2f"%(percentage_T)+"%"
 props = dict(boxstyle='square', facecolor='white', alpha=0.5)
@@ -853,14 +857,14 @@ plt.plot(np.full((17), 10), np.arange(17), color="black", linestyle = "--")
 plt.plot(np.full((17), 12), np.arange(17), color="black", linestyle = "--")
 
 # Text
-plt.text(6.6, 15, "MSE: 2.35e-2", fontsize=16)
-plt.text(6.55, 14.7, " %.2f"%(percentage_S)+"%", fontsize=16)
+plt.text(6.6, 15, "2.35e-2", fontsize=23)
+plt.text(6.52, 14.55, " %.2f"%(percentage_S)+"%", fontsize=23)
 
-plt.text(10.1, 15, "MSE: 5.41e-4", fontsize=16)
-plt.text(10.05, 14.7, " %.2f"%(percentage_M)+"%", fontsize=16)
+plt.text(10.1, 15, "5.41e-4", fontsize=23)
+plt.text(10, 14.55, " %.2f"%(percentage_M)+"%", fontsize=23)
 
-plt.text(12.1, 15, "MSE: 1.07e-3", fontsize=16)
-plt.text(12.05, 14.7, " %.2f"%(percentage_L)+"%", fontsize=16)
+plt.text(12.1, 15, "1.07e-3", fontsize=23)
+plt.text(12.05, 14.55, " %.2f"%(percentage_L)+"%", fontsize=23)
 
 textstr = "MSE: 8.12e-3 \n%.2f"%(percentage_T)+"%"
 props = dict(boxstyle='square', facecolor='white', alpha=0.5)
@@ -960,26 +964,26 @@ plt.plot(np.full((17), 10), np.arange(17), color="black", linestyle = "--")
 plt.plot(np.full((17), 12), np.arange(17), color="black", linestyle = "--")
 
 # Text
-plt.text(6.6, 15, "MSE: 9.35e-3", fontsize=16)
-plt.text(6.55, 14.7, " %.2f"%(percentage_S)+"%", fontsize=16)
+plt.text(6.6, 15, "9.35e-3", fontsize=23)
+plt.text(6.52, 14.55, " %.2f"%(percentage_S)+"%", fontsize=23)
 
-plt.text(10.1, 15, "MSE: 1.26e-3", fontsize=16)
-plt.text(10.05, 14.7, " %.2f"%(percentage_M)+"%", fontsize=16)
+plt.text(10.1, 15, "1.26e-3", fontsize=23)
+plt.text(10, 14.55, " %.2f"%(percentage_M)+"%", fontsize=23)
 
-plt.text(12.1, 15, "MSE: 6.41e-4", fontsize=16)
-plt.text(12.05, 14.7, " %.2f"%(percentage_L)+"%", fontsize=16)
+plt.text(12.1, 15, "6.41e-4", fontsize=23)
+plt.text(12.05, 14.55, " %.2f"%(percentage_L)+"%", fontsize=23)
 
 textstr = "MSE: 3.92e-3 \n%.2f"%(percentage_T)+"%"
 props = dict(boxstyle='square', facecolor='white', alpha=0.5)
 plt.text(12.25, 7.2, textstr, bbox=props, horizontalalignment="left", fontsize=24)
 
-cb_ax3 = fig.add_axes([.906, .32, .015, .18])
+cb_ax3 = fig.add_axes([.906, .315, .015, .18])
 fig.colorbar(plt.hexbin(true, pred, bins="log", mincnt=1, gridsize=220, cmap="plasma"),
              orientation='vertical',cax=cb_ax3, ).set_label("Number of Galaxies", rotation=270, fontsize=27, labelpad=25)
 
 ######################################################## TNG300 ###################################################################
 
-plt.subplot(4,3,10).set(xticks=[])
+plt.subplot(4,3,10)
 #  Symbolic Model
 
 ############################### DATA #############################
@@ -1053,7 +1057,7 @@ plt.hexbin(total_true, total_pred, bins="log", mincnt=1, gridsize=220, cmap="pla
 #cbar = plt.colorbar(pad=0.007)
 #cbar.set_label("Number of Galaxies", rotation=270, fontsize=17, labelpad=25)
 plt.ylabel("Predicted [$log_{10}(M_\odot /h)$]", fontsize=29)
-#plt.xlabel("Truth [$log_{10}(M_\odot /h)$]", fontsize=18
+plt.xlabel("Truth [$log_{10}(M_\odot /h)$]", fontsize=29)
 
 # y=x line
 min = 6
@@ -1069,14 +1073,14 @@ plt.plot(np.full((17), 10), np.arange(17), color="black", linestyle = "--")
 plt.plot(np.full((17), 12), np.arange(17), color="black", linestyle = "--")
 
 # Text
-plt.text(6.6, 15, "MSE: 1.25e-3", fontsize=16)
-plt.text(6.55, 14.7, " %.2f"%(percentage_S)+"%", fontsize=16)
+plt.text(6.6, 15, "1.25e-3", fontsize=23)
+plt.text(6.52, 14.55, " %.2f"%(percentage_S)+"%", fontsize=23)
 
-plt.text(10.1, 15, "MSE: 1.03e-3", fontsize=16)
-plt.text(10.05, 14.7, " %.2f"%(percentage_M)+"%", fontsize=16)
+plt.text(10.1, 15, "1.03e-3", fontsize=23)
+plt.text(10, 14.55, " %.2f"%(percentage_M)+"%", fontsize=23)
 
-plt.text(12.1, 15, "MSE: 1.24e-3", fontsize=16)
-plt.text(12.05, 14.7, " %.2f"%(percentage_L)+"%", fontsize=16)
+plt.text(12.1, 15, "1.24e-3", fontsize=23)
+plt.text(12.05, 14.55, " %.2f"%(percentage_L)+"%", fontsize=23)
 
 textstr = "MSE: 1.07e-3 \n%.2f"%(percentage_T)+"%"
 props = dict(boxstyle='square', facecolor='white', alpha=0.5)
@@ -1084,7 +1088,7 @@ plt.text(12.25, 7.2, textstr, bbox=props, horizontalalignment="left", fontsize=2
 
 #####################################################################################################################
 # NN (3vars)
-plt.subplot(4,3,11).set(xticks=[], yticks=[])
+plt.subplot(4,3,11).set(yticks=[])
 
 ############################### DATA #############################
 true = np.log10(np.load("tng300_3var_true.npy"))
@@ -1160,7 +1164,7 @@ plt.hexbin(true, pred, bins="log", mincnt=1, gridsize=220, cmap="plasma")
 #cbar = plt.colorbar(pad=0.007)
 #cbar.set_label("Number of Galaxies", rotation=270, fontsize=17, labelpad=25)
 #plt.ylabel("Predicted [$log_{10}(M_\odot /h)$]", fontsize=29)
-#plt.xlabel("Truth [$log_{10}(M_\odot /h)$]", fontsize=18
+plt.xlabel("Truth [$log_{10}(M_\odot /h)$]", fontsize=29)
 
 # y=x line
 min = 6
@@ -1176,14 +1180,14 @@ plt.plot(np.full((17), 10), np.arange(17), color="black", linestyle = "--")
 plt.plot(np.full((17), 12), np.arange(17), color="black", linestyle = "--")
 
 # Text
-plt.text(6.6, 15, "MSE: 3.92e-3", fontsize=16)
-plt.text(6.55, 14.7, " %.2f"%(percentage_S)+"%", fontsize=16)
+plt.text(6.6, 15, "3.92e-3", fontsize=23)
+plt.text(6.52, 14.55, " %.2f"%(percentage_S)+"%", fontsize=23)
 
-plt.text(10.1, 15, "MSE: 4.94e-4", fontsize=16)
-plt.text(10.05, 14.7, " %.2f"%(percentage_M)+"%", fontsize=16)
+plt.text(10.1, 15, "4.94e-4", fontsize=23)
+plt.text(10.05, 14.55, " %.2f"%(percentage_M)+"%", fontsize=23)
 
-plt.text(12.1, 15, "MSE: 7.44e-4", fontsize=16)
-plt.text(12.05, 14.7, " %.2f"%(percentage_L)+"%", fontsize=16)
+plt.text(12.1, 15, "7.44e-4", fontsize=23)
+plt.text(12.05, 14.55, " %.2f"%(percentage_L)+"%", fontsize=23)
 
 textstr = "MSE: 8.80e-4 \n%.2f"%(percentage_T)+"%"
 props = dict(boxstyle='square', facecolor='white', alpha=0.5)
@@ -1191,7 +1195,7 @@ plt.text(12.25, 7.2, textstr, bbox=props, horizontalalignment="left", fontsize=2
 
 ###############################################################################################################
 # NN (11vars)
-plt.subplot(4,3,12).set(xticks=[], yticks=[])
+plt.subplot(4,3,12).set(yticks=[])
 
 ############################### DATA #############################
 true = np.log10(np.load("true_tng300_z_0.npy"))
@@ -1267,7 +1271,7 @@ plt.hexbin(true, pred, bins="log", mincnt=1, gridsize=220, cmap="plasma")
 #cbar = plt.colorbar(pad=0.007)
 #cbar.set_label("Number of Galaxies", rotation=270, fontsize=17, labelpad=25)
 #plt.ylabel("Predicted [$log_{10}(M_\odot /h)$]", fontsize=29)
-#plt.xlabel("Truth [$log_{10}(M_\odot /h)$]", fontsize=18
+plt.xlabel("Truth [$log_{10}(M_\odot /h)$]", fontsize=29)
 
 # y=x line
 min = 6
@@ -1283,22 +1287,23 @@ plt.plot(np.full((17), 10), np.arange(17), color="black", linestyle = "--")
 plt.plot(np.full((17), 12), np.arange(17), color="black", linestyle = "--")
 
 # Text
-plt.text(6.6, 15, "MSE: 6.51e-4", fontsize=16)
-plt.text(6.55, 14.7, " %.2f"%(percentage_S)+"%", fontsize=16)
+plt.text(6.6, 15, "6.51e-4", fontsize=23)
+plt.text(6.52, 14.55, " %.2f"%(percentage_S)+"%", fontsize=23)
 
-plt.text(10.1, 15, "MSE: 3.94e-4", fontsize=16)
-plt.text(10.05, 14.7, " %.2f"%(percentage_M)+"%", fontsize=16)
+plt.text(10.1, 15, "3.94e-4", fontsize=23)
+plt.text(10, 14.55, " %.2f"%(percentage_M)+"%", fontsize=23)
 
-plt.text(12.1, 15, "MSE: 4.35e-4", fontsize=16)
-plt.text(12.05, 14.7, " %.2f"%(percentage_L)+"%", fontsize=16)
+plt.text(12.1, 15, "4.35e-4", fontsize=23)
+plt.text(12.05, 14.55, "%.2f"%(percentage_L)+"%", fontsize=23)
 
 textstr = "MSE: 4.25e-4 \n%.2f"%(percentage_T)+"%"
 props = dict(boxstyle='square', facecolor='white', alpha=0.5)
 plt.text(12.25, 7.2, textstr, bbox=props, horizontalalignment="left", fontsize=24)
 
-cb_ax4 = fig.add_axes([.906, .13, .015, .18])
+cb_ax4 = fig.add_axes([.906, .128, .017, .18])
 fig.colorbar(plt.hexbin(true, pred, bins="log", mincnt=1, gridsize=220, cmap="plasma"),
              orientation='vertical',cax=cb_ax4, ).set_label("Number of Galaxies", rotation=270, fontsize=27, labelpad=25)
 
 plt.subplots_adjust(wspace=0, hspace=0)
-plt.show()
+
+#plt.savefig("all_models.pdf", dpi=1200, bbox_inches='tight')
